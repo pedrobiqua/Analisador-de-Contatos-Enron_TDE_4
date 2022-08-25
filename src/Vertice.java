d762c4673a9b4109a1c9ddbeecddd68e4099902c
Pedro <pedrobiqua@gmail.com>
2022-08-25 09:13:17 -0300

    first commit

---
+++ src/Vertice.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Vertice {
+    public int dado;
+    public int informacao;
+    public ArrayList<Adjacencia> AdjacenciaEntrada;
+    public ArrayList<Adjacencia> AdjacenciaSaida;
+
+    public Vertice(int valor) {
+        this.dado = valor;
+        AdjacenciaEntrada = new ArrayList<Adjacencia>();
+        AdjacenciaSaida = new ArrayList<Adjacencia>();
+    }
+
+    public void adicionarAdjacenciaEntrada(Adjacencia Adjacencia){
+        AdjacenciaEntrada.add(Adjacencia);
+    }
+
+    public void adicionarAdjacenciaSaida(Adjacencia Adjacencia){
+        AdjacenciaSaida.add(Adjacencia);
+    }
+}
